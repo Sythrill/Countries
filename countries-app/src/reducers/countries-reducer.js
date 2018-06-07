0e60bde796cae6974fa968607f865cd552b61df8
Sythrill <ksciwiarska@gmail.com>
2018-06-07 19:30:06 +0200

    change functions to arrow functions

--- countries-app/src/reducers/countries-reducer.js
+++ countries-app/src/reducers/countries-reducer.js
@@ -5,10 +5,10 @@ const initialState = {
     countries: countriesData
 };
 
-const countriesReducer = function (state = initialState, action) {
+const countriesReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_COUNTRIES:
-            return Object.assign({}, state, {countries: state.countries})
+            return {...state, countries: state.countries}
     }
 
     return state;
